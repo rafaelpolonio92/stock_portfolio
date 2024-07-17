13eccea7e50a9e5251d69c98a3c607dd51ce2ae0
Rafael Polonio <rafael.polonio@gmail.com>
2024-07-17 16:45:34 -0300

    settings

--- stock_portfolio/settings.py
+++ stock_portfolio/settings.py
@@ -91,7 +91,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'api.User'
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
